3dd494ebd7d63b63b72e668a6d31a19858cf0e98
Ada <adrianna.bielicz@pl.abb.com>
2018-06-20 14:12:59 +0000

    rename dir

---
+++ 01GettingStarted/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //Hello world programm
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            //Waiting for user input to close console
+            Console.ReadKey();
+        }
+    }
+}
