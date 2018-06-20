64480481991e7eb46e3ef3fe2a29b5ad9c6751f9
Ada <adrianna.bielicz@pl.abb.com>
2018-06-20 14:53:06 +0000

    update content of programm dir

--- 01GettingStarted/ConsoleApp1/Program.cs
+++ 01GettingStarted/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            //Waiting for user input to close console
+            //Waiting for user input enter key to close console
             Console.ReadKey();
         }
     }
