3da960fede9eea8a7296c67281a6ce6721f4cbb6
Ada <adrianna.bielicz@pl.abb.com>
2018-06-20 14:59:51 +0000

    1step basis

---
+++ 02_Basics/02_Basics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_Basics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Jane";
+            string lastName = "Doe";
+
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+
+            Console.WriteLine("Please enter a new first name and press enter key:");
+            /*
+             * In Csharp, strings are immutable, which means that strings are never changed after they have been created. 
+             * When using methods which changes a string, the actual string is not changed - a new string is returned instead.
+            */
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("New name: " + firstName + " " + lastName);
+
+            Console.ReadLine();
+        }
+    }
+}
