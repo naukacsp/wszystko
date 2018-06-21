b3bef02d78f0d1cb497d4d71989025df6d166460
Ada <adrianna.bielicz@pl.abb.com>
2018-06-21 08:25:48 +0000

    added controlling structures

--- 02_Basics/02_Basics/Program.cs
+++ 02_Basics/02_Basics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,15 +13,47 @@ namespace _02_Basics
             string lastName = "Doe";
 
             Console.WriteLine("Name: " + firstName + " " + lastName);
+            while (true)
+            {
+                Console.Write("Enter the new first name and press enter key:: ");
+                firstName = Console.ReadLine();
 
-            Console.WriteLine("Please enter a new first name and press enter key:");
+                if (Regex.IsMatch(firstName, @"^[a-zA-Z]+$")) //better way to do this!? this won't work enter value 
+                {   
+
+                    break;
+                }
+                Console.WriteLine("Please enter characters value!");
+            }
             /*
              * In Csharp, strings are immutable, which means that strings are never changed after they have been created. 
              * When using methods which changes a string, the actual string is not changed - a new string is returned instead.
             */
-            firstName = Console.ReadLine();
+            
+            //firstName = Console.ReadLine();
 
             Console.WriteLine("New name: " + firstName + " " + lastName);
+
+            int number;
+
+            Console.WriteLine("Please enter a number between 0 and 10:");
+            //line below make bugs? why is that?
+            //number = int.Parse(Console.ReadLine());
+            //other solution
+            while (true)
+            {
+                Console.Write("Enter the number : ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter an integer value!");
+            }
+
+            if ((number > 10) || (number < 0))
+                Console.WriteLine("Hey! The number should be 0 or more and 10 or less!");
+            else
+                Console.WriteLine("Good job!");
 
             Console.ReadLine();
         }
