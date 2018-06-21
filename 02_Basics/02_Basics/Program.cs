386e0ad98a386d7b0554ccd60b93c09dc0a5097c
Jakub Krupka <jakub.krupka@pl.abb.com>
2018-06-21 11:29:06 +0200

    small fix

--- 02_Basics/02_Basics/Program.cs
+++ 02_Basics/02_Basics/Program.cs
@@ -18,28 +18,19 @@ namespace _02_Basics
                 Console.Write("Enter the new first name and press enter key:: ");
                 firstName = Console.ReadLine();
 
-                if (Regex.IsMatch(firstName, @"^[a-zA-Z]+$")) //better way to do this!? this won't work enter value 
-                {   
-
+                if (Regex.IsMatch(firstName, @"^[a-zA-Z]+$"))
+                {
                     break;
                 }
                 Console.WriteLine("Please enter characters value!");
             }
-            /*
-             * In Csharp, strings are immutable, which means that strings are never changed after they have been created. 
-             * When using methods which changes a string, the actual string is not changed - a new string is returned instead.
-            */
-            
-            //firstName = Console.ReadLine();
 
             Console.WriteLine("New name: " + firstName + " " + lastName);
 
             int number;
 
             Console.WriteLine("Please enter a number between 0 and 10:");
-            //line below make bugs? why is that?
-            //number = int.Parse(Console.ReadLine());
-            //other solution
+
             while (true)
             {
                 Console.Write("Enter the number : ");
@@ -50,10 +41,14 @@ namespace _02_Basics
                 Console.WriteLine("Please enter an integer value!");
             }
 
-            if ((number > 10) || (number < 0))
+            if (number > 10 || number < 0)
+            {
                 Console.WriteLine("Hey! The number should be 0 or more and 10 or less!");
+            }
             else
+            {
                 Console.WriteLine("Good job!");
+            }
 
             Console.ReadLine();
         }
